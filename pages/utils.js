def802797d2f015976a1c50273d5d23683cd181c
Kevin Oh <aflashyrhetoric@gmail.com>
2021-01-04 13:08:37 -0500

    Moved more things around

--- pages/utils.js
+++ pages/utils.js
@@ -1,18 +1,18 @@
-import { alphabet } from './vars'
+// import { alphabet } from './vars'
 
-export const randomNumber = (max) => {
-  return Math.floor(Math.random() * max)
-}
+// export const randomNumber = (max) => {
+//   return Math.floor(Math.random() * max)
+// }
 
-export const randomLetter = () => {
-  const randomNumber = Math.floor(Math.random() * 26)
-  return alphabet.lower[randomNumber]
-}
+// export const randomLetter = () => {
+//   const randomNumber = Math.floor(Math.random() * 26)
+//   return alphabet.lower[randomNumber]
+// }
 
-export const randomLetterN = (n) => {
-  let letters = []
-  for (let i = 0; i < n; i++) {
-    letters.push(randomLetter())
-  }
-  return letters
-}
+// export const randomLetterN = (n) => {
+//   let letters = []
+//   for (let i = 0; i < n; i++) {
+//     letters.push(randomLetter())
+//   }
+//   return letters
+// }
